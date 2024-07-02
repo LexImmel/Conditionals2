13af3f945796cbf378826c94d0b05d8caf26f9a2
LexImmel <juffin91@gmail.com>
2024-07-02 21:31:36 +0500

    Initial comm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,66 @@
-
 public class Main {
     public static void main(String[] args) {
+    //Задача 1
+        int clientOS =  1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        //Задача 2
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        //Задача 3
+        int year = 2000;
+        if (year <= 1584) {
+            System.out.println("Високосные года еще не введены!");
+        } else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+                System.out.println(year + " год не является високосным");
+            }
+        //Задача 4
+        int deliveryDistance = 95;
+        short deliveryDuration = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + deliveryDuration);
+        } else if (deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: " + (deliveryDuration + 1));
+        } else if (deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: " + (deliveryDuration + 2));
+        } else {
+            System.out.println("Свыше 100 км доставки нет.");
+        }
+        //Задача 5
+        short monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Это зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Это весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Это лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Это осень");
+                break;
+            default:
+                System.out.println("Некорректный номер месяца");
 
+        }
     }
 }
